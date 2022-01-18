cb4e42aeb1981e7128a1244251121f616341dc94
牛客142508207号 <2636417147@qq.com>
2022-01-18 15:03:26 +0800

    项目初始化

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+      </Head>
+
+      <main className={styles.main}>
+
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
