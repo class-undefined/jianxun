4ff17e39353773ced5c5306c44fd0f7b309c12fa
野漫横江 <1399226280@qq.com>
2022-01-20 22:31:38 +0800

    feat: render

--- my-app/component/ToolBar/ToolBar.tsx
+++ my-app/component/ToolBar/ToolBar.tsx
@@ -3,14 +3,13 @@ import styles from './ToolBar.module.css'
 import { getArticleData } from "../../api/article"
 import { useEffect, useState } from "react"
 import { Response, StatusCode } from "../../utils/api/response/type"
-export interface ToolBarRenderData {
-    comment: number, // 评论
-    like: number, // 点赞
-    share: number // 分享
+export interface ToolBarAction {
+    icon: string,
+    content: string,
+    onClick?: () => void
 }
 export interface ToolBarProps {
     className?: string,
-    renderData?: ToolBarRenderData
 }
 
 /**
@@ -18,9 +17,17 @@ export interface ToolBarProps {
  * @returns 
  */
 export const ToolBar: React.FC<ToolBarProps> = () => {
-    const [data, setData] = useState({comment: 0, like: 0, share: 0})
+    const [data, setData] = useState({comment: '-', like: '-', share: '-'})
     const articleId = "123123"
     const {comment, like, share} = data
+    const renderData: ToolBarAction[] = [
+        {icon: 'comment', content: comment},
+        {icon: 'like', content: like},
+        {icon: 'share', content: share},
+        {icon: 'issue', content: "反馈"},
+        {icon: 'favorite', content: "收藏"},
+        {icon: 'voice', content: "朗读"}
+    ] 
     useEffect(() => {
         getArticleData(articleId).then((response: unknown) => {
             const {code, data, message} = response as Response
@@ -34,24 +41,15 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
     return (
         <div className={styles['ToolBar-contaienr']}>
             <div className={styles['ToolBar-actions']}>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='comment' content={comment + ''}/>
-                </div>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='like' content={like + ''}/>
-                </div>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='share' content={share + ''}/>
-                </div>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='issue' content="反馈"/>
-                </div>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='favorite' content="收藏"/>
-                </div>
-                <div className={styles['ToolBar-action-item']}>
-                    <ToolBarItem icon='voice' content="朗读"/>
-                </div>
+                {
+                    renderData.map((actionItem: ToolBarAction) => {
+                        return (
+                            <div key={actionItem.icon} className={styles['ToolBar-action-item']}>
+                                <ToolBarItem {...actionItem} />
+                            </div>
+                        )
+                    })
+                }
             </div>
             <div className={styles['ToolBar-expand-btn']}>btn</div>
         </div>
