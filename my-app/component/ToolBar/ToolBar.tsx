88a15d05b7f2934b3acee311568b3d88ea8cbfd2
wild-sky <1399226280@qq.com>
2022-01-21 13:40:12 +0800

    feat: 对`renderData` 数据结构增加`fixed`字段决定是否在normal时显示, 加入点击expendBtn进行的ToolBar伸缩

--- my-app/component/ToolBar/ToolBar.tsx
+++ my-app/component/ToolBar/ToolBar.tsx
@@ -1,11 +1,17 @@
+/**
+ * @author: 野漫横江
+ */
 import { IconActionProps, ToolBarItem } from "./children/ToolBarIcon/ToolBarItem"
 import styles from './ToolBar.module.css'
 import { getArticleData } from "../../api/article"
 import { useEffect, useState } from "react"
 import { Response, StatusCode } from "../../utils/api/response/type"
+import { SvgIcon } from "../SvgIcon/SvgIcon"
+import { ClassNameBuilder } from "../../utils/style"
 export interface ToolBarAction {
     icon: string,
     content: string,
+    fixed: boolean,
     onClick?: () => void
 }
 export interface ToolBarProps {
@@ -18,16 +24,25 @@ export interface ToolBarProps {
  */
 export const ToolBar: React.FC<ToolBarProps> = () => {
     const [data, setData] = useState({comment: '-', like: '-', share: '-'})
+    const [expendBtnStyle, setExpendBtnStyle] = useState({
+        rootClassName: styles['ToolBar-contaienr'],
+        actionsClassName: styles['ToolBar-actions'],
+        iconName: "more",
+        isExpend: false
+    })
+    const rootClassNameController = ClassNameBuilder.from([styles['ToolBar-contaienr'], styles['ToolBar-contaienr-expend']]).build()
+    const actionsClassNameController = ClassNameBuilder.from([styles['ToolBar-actions'], styles['ToolBar-actions-expend']]).build()
     const articleId = "123123"
     const {comment, like, share} = data
     const renderData: ToolBarAction[] = [
-        {icon: 'comment', content: comment},
-        {icon: 'like', content: like},
-        {icon: 'share', content: share},
-        {icon: 'issue', content: "反馈"},
-        {icon: 'favorite', content: "收藏"},
-        {icon: 'voice', content: "朗读"}
+        {icon: 'comment', content: comment, fixed: true},
+        {icon: 'like', content: like, fixed: true},
+        {icon: 'share', content: share, fixed: true},
+        {icon: 'issue', content: "反馈", fixed: false},
+        {icon: 'favorite', content: "收藏", fixed: false},
+        {icon: 'voice', content: "朗读", fixed: false}
     ] 
+    const {isExpend, iconName, rootClassName, actionsClassName} = expendBtnStyle
     useEffect(() => {
         getArticleData(articleId).then((response: unknown) => {
             const {code, data, message} = response as Response
@@ -37,12 +52,25 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
             }
             setData({...data})
         })
-    }, [articleId])
+    }, [articleId, isExpend])
+    
+    const expendHandle = () => {
+        
+        setExpendBtnStyle({
+            isExpend: !isExpend,
+            rootClassName: rootClassNameController(!isExpend),
+            iconName: !isExpend ? "down" : "more",
+            actionsClassName: actionsClassNameController(!isExpend)
+        })
+    }
     return (
-        <div className={styles['ToolBar-contaienr']}>
-            <div className={styles['ToolBar-actions']}>
+        <div className={rootClassName}>
+            <div className={actionsClassName}>
                 {
-                    renderData.map((actionItem: ToolBarAction) => {
+                    renderData.filter((actionItem => {
+                        if (actionItem.fixed) return true
+                        return isExpend
+                    })).map((actionItem: ToolBarAction) => {
                         return (
                             <div key={actionItem.icon} className={styles['ToolBar-action-item']}>
                                 <ToolBarItem {...actionItem} />
@@ -51,7 +79,9 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
                     })
                 }
             </div>
-            <div className={styles['ToolBar-expand-btn']}>btn</div>
+            <div className={styles['ToolBar-expand-btn']} onClick={expendHandle}>
+                <SvgIcon key={iconName} iconClass={iconName} height={32}/>
+            </div>
         </div>
     )
 }
