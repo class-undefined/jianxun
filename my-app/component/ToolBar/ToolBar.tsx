2891d8135353439ab656b38cc69b85dd62e5efa3
wild-sky <1399226280@qq.com>
2022-01-22 22:54:05 +0800

    feat: 新增api `ToolBarEffect` 用于控制ToolBar的更新，废弃`ToolBarModal`

--- my-app/component/ToolBar/ToolBar.tsx
+++ my-app/component/ToolBar/ToolBar.tsx
@@ -9,7 +9,7 @@ import { Response, StatusCode } from "../../utils/api/response/type"
 import { SvgIcon } from "../SvgIcon/SvgIcon"
 import { ClassNameBuilder } from "../../utils/style"
 import { Article } from "../../type/article"
-import { ToolBarModal } from "./components/ToolBarModal/ToolBarModal"
+import { ToolBarEffect } from "./api/ToolBarEffect"
 export interface ToolBarAction {
     icon: string,
     content: string | number,
@@ -20,12 +20,30 @@ export interface ToolBarProps {
     className?: string,
 }
 
+const mockArticleData:Article = {
+    id: "13215465163521",
+    image: "https://www.baidu.com/",
+    title: "我怎样才能做到？",
+    content: "在生活中，在工作中，在学习中，你是否总是告诉自己“我做不到？”如果你保持着这样的局限性思维，那么它就会阻止你的大脑进行创造性的思考，因此就无法找到新方法让自己变得更好。所以我们应该有着“我怎样才能做到”的思维，激发大脑进行创造性思考，并提出更多建设性的意见，就能推动我们的成长和进步，同时，我们的心态也会更加的积极进取，而不是自怨自艾。",
+    tags: [
+        {id: "21341654", name: "炫酷脑科学"},
+        {id: "21341655", name: "福报来了"},
+    ],
+    comment: 25,
+    like: {
+        value: 1608,
+        isDone: false
+    },
+    share: 53
+}
+
 /**
  * 使用flex、grid布局，ToolBar控件分normal、expend两个状态，默认为normal，点击expendBtn切换状态。
  * @returns 
  */
 export const ToolBar: React.FC<ToolBarProps> = () => {
-    const [data, setData] = useState({ comment: 0, like: 0, share: 0 })
+    const [article, setArticle] = useState(mockArticleData)
+    const [Template, setTemplate] = useState((<template></template>))
     const [expendBtnStyle, setExpendBtnStyle] = useState({
         rootClassName: styles['ToolBar-root'],
         containerClassName: styles['ToolBar-contaienr'],
@@ -41,7 +59,8 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
     const actionsClassNameController = ClassNameBuilder.from([styles['ToolBar-actions'], styles['ToolBar-actions-expend']]).build()
     const expendBtnClassNameController = ClassNameBuilder.from([styles['ToolBar-expand-btn'], styles['ToolBar-expand-btn-expend']]).build()
     const articleId = "123123"
-    const { comment, like, share } = data
+    const { comment, share } = article
+    const like = article.like.value
     const renderData: ToolBarAction[] = [
         { icon: 'comment', content: comment, fixed: true },
         { icon: 'like', content: like, fixed: true },
@@ -65,10 +84,16 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
                 alert("error1")
                 return
             }
-            const {comment, like, share} = (data as Article)
-            setData({ comment, like: like.value, share })
+            setArticle(data as Article)
         })
     }, [articleId])
+    
+    ToolBarEffect.useHandle(article, (e) => {
+        const {sender, component, article} = e
+        if (article !== null) setArticle(article)
+        const F = component
+        setTemplate(<F/>)
+    })
     const expendHandle = () => {
         const nextIsExpend = !isExpend
         setExpendBtnStyle({
@@ -81,7 +106,6 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
             iconName: nextIsExpend ? "down" : "more",
         })
     }
-    const Template = (ToolBarModal.template()) as React.FC<any>
     return (
         <div className={rootClassName}>
             <div className={containerClassName}>
@@ -104,7 +128,7 @@ export const ToolBar: React.FC<ToolBarProps> = () => {
                 </div>
             </div>
             <div className={backGroundClassName}><div></div></div>
-            <Template />
+            {Template}
         </div>
     )
 }
---
+++ my-app/component/ToolBar/api/ToolBarEffect/index.ts
@@ -0,0 +1,53 @@
+import mitt, {Emitter} from "mitt";
+import React from "react";
+import { Article } from "../../../../type/article";
+
+interface HandlerData {
+    sender: string, // 事件发送者
+    component: React.FC, // 发送数据
+    article: Article | null
+}
+
+
+type EventType = {
+    render: HandlerData
+}
+
+/**
+ * 用于通知ToolBar进行渲染指定组件
+ */
+export class ToolBarEffect {
+    private static emitter: Emitter<EventType> = mitt<EventType>() // 事件
+
+    private static article: Article | null = null
+
+    private static isRegister = false
+
+    private constructor() {
+        throw new Error("为了避免事件全局污染，ToolBarEffect构造函数已被禁用，禁止调用。")
+    }
+
+    /**
+     * 监听渲染事件
+     * @param article 文章数据
+     * @param callback 处理更新消息的回调函数
+     */
+    public static useHandle(article: Article, callback: (e: HandlerData) => void) {
+        this.isRegister = true
+        this.article = article
+        this.emitter.off("render")
+        this.emitter.on("render", callback)
+    }
+
+    /**
+     * 通知ToolBar进行渲染传递过来的组件
+     * @param sender 消息发送者身份
+     * @param callback 返回需要对ToolBar进行更新的回调函数
+     */
+    public static render(sender: string, callback: (article: Article) => React.FC ) {
+        if (this.isRegister === false) throw new Error("监听函数在ToolBar组件中尚未注册运行，调用该函数的生命周期过早！");
+        
+        const F = callback(this.article as Article)
+        this.emitter.emit("render", {sender, component: F, article: this.article})
+    }
+}
