db6bd502aad9b9d30c64f97db43ec5d1235622cd
wild-sky <1399226280@qq.com>
2022-01-23 12:31:44 +0800

    config:

--- my-app/test/ToolBarEffect/index.ts
+++ my-app/test/ToolBarEffect/index.ts
@@ -2,7 +2,7 @@ import { TestToolBarEffect } from "../../example/ToolBarEffect"
 
 export const ToolBarEffectTest = (isRun: boolean) => {
     if (!isRun) return
-    TestToolBarEffect("123123132") // 如果取消注释，则触发生命周期调用检查
+    // TestToolBarEffect("123123132") // 如果取消注释，则触发生命周期调用检查
     setTimeout(()=>{
         TestToolBarEffect("123123132")
     }, 1000)
